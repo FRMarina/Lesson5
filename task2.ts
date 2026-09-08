f872ca102cb69ea0f1e9f85dc33b854cbc859d51
FRMarina <m.fedchikrolich@gmail.com>
2026-09-08 21:17:55 +0300

    feat: second task was resolved

--- task2.ts
+++ task2.ts
@@ -1,1 +1,12 @@
 // Напишите самовызывающиеся функцию подсчета факториала числа: число должно передаваться как параметр функции
+const result = (function countFactorial(n: bigint): bigint | void {
+  if (n < 0n) {
+    console.error("Should be above 0");
+    return;
+  }
+  if (n <= 1n) {
+    return 1n;
+  }
+  return n * (countFactorial(n - 1n) ?? 1n);
+})(1000n);
+console.log(result);
